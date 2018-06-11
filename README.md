bb0f6048e4b7f1131a4398414b44cce4d20590b6
Richard Giddings <mail@richardgiddings.co.uk>
2018-06-11 22:03:55 +0100

    Added search by date functionality

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # README
 
-Get activities from Strava for an athlete. Uses [social-auth-app-django](https://github.com/python-social-auth/social-app-django) to authenticate a user then [stravalib](https://github.com/hozn/stravalib) to retrieve the activities for that user. Activities are displayed on the page with pagination.
+Get activities from Strava for an athlete. Uses [social-auth-app-django](https://github.com/python-social-auth/social-app-django) to authenticate a user then [stravalib](https://github.com/hozn/stravalib) to retrieve the activities for that user. Activities are displayed on the page with pagination. 
+
+The first time you visit the site there are no activities shown. If you just specifiy a From date then activities after that date are retrieved. If you just specify a To date then activities up until that date are retrieved. If you specify both you get activities in that range of dates. If you specify neither then nothing is retrieved.
 
 ![Alt text](site.png?raw=true "site")
 
--- core/views.py
+++ core/views.py
@@ -11,38 +11,68 @@ from stravalib.client import Client
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+from datetime import datetime
+
 @login_required
 def home(request):
 
-    page = request.GET.get('page')
+    from_date = request.GET.get('from_date')
+    to_date = request.GET.get('to_date')
 
-    # get access token
-    social = request.user.social_auth.get(provider='strava')
-    token = social.extra_data['access_token']
-
-    # get activity details
-    client = Client()
-    client.access_token = token
     activities = []
-    for activity in client.get_activities(after="2010-01-01T00:00:00Z"):
-        activities.insert(0, 
-            { 
-                "id": activity.id,
-                "name": activity.name,
-                "link": "https://www.strava.com/activities/{}".format(activity.id),
-                "date": activity.start_date_local,
-                "athlete_count": activity.athlete_count,
-            }
-        )
+      
+    if from_date or to_date:
+        if from_date:
+            from_datetime = datetime.strptime(from_date + " 00:00:00", "%d/%m/%Y %H:%M:%S")
+        if to_date:
+            to_datetime = datetime.strptime(to_date + " 23:59:59", "%d/%m/%Y %H:%M:%S")
 
-    paginator = Paginator(activities, 20) # Show n results per page
-    try:
-        activities = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        activities = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        activities = paginator.page(paginator.num_pages)
+        page = request.GET.get('page')
 
-    return render(request, 'core/home.html', context={'activities': activities})
+        # get access token
+        social = request.user.social_auth.get(provider='strava')
+        token = social.extra_data['access_token']
+
+        # get activity details
+        client = Client()
+        client.access_token = token
+
+        if from_date and to_date:
+            query = client.get_activities(before=to_datetime, after=from_datetime)
+        elif from_date: 
+            query = client.get_activities(after=from_datetime)
+        else: # to_date
+            query = client.get_activities(before=to_datetime)
+
+        for activity in query:
+            if from_date and not to_date:
+                index = 0
+            else:
+                index = len(activities)
+
+            activities.insert(index,
+                { 
+                    "id": activity.id,
+                    "name": activity.name,
+                    "link": "https://www.strava.com/activities/{}".format(activity.id),
+                    "date": activity.start_date_local,
+                    "athlete_count": activity.athlete_count,
+                }
+            )
+
+        paginator = Paginator(activities, 20) # Show n results per page
+        try:
+            activities = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            activities = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            activities = paginator.page(paginator.num_pages)
+
+    return render(request, 'core/home.html', 
+                            context={
+                                'activities': activities,
+                                'from_date': from_date,
+                                'to_date': to_date,
+                            })
